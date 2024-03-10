6d50fe72ac323dba197357c08810cb81df6f914e
Kashish Rohira <kashishrohira2004@gmail.com>
2024-03-09 16:10:13 -0800

    news

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "@/frontend/sections/Hero";
 import Navbar from "@/frontend/components/Navbar";
 
+
 export default function Home() {
   return (
     <div>
